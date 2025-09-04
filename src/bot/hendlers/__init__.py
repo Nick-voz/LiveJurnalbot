b1b3e6f83654f2fc45919f8e7de25500b731a864
Nick-voz <nik.vozisow@gmail.com>
2025-09-04 08:50:28 +0300

    feat(handlers): Introduce reminder strategies module
    - Create new module `src/bot/hendlers/reminder_strategies.py` to house handlers for setting up reminder strategies.
    - Add a placeholder `register` function to the new module, which will be populated with conversation handlers later.
    - Update `src/bot/hendlers/__init__.py` to import and call the `register` function from the `reminder_strategies` module, integrating it into the bot's handler registration process.

--- src/bot/hendlers/__init__.py
+++ src/bot/hendlers/__init__.py
@@ -1,9 +1,13 @@
 from telegram.ext import Application
 
 from src.bot.hendlers.base import register as base_register
+from src.bot.hendlers.reminder_strategies import (
+    register as reminder_strategies_register,
+)
 from src.bot.hendlers.scenarios import register as scenarios_register
 
 
 def register(app: Application):
     base_register(app)
     scenarios_register(app)
+    reminder_strategies_register(app)
---
+++ src/bot/hendlers/reminder_strategies.py
@@ -0,0 +1,10 @@
+from telegram import Update
+from telegram.ext import Application
+from telegram.ext import CommandHandler
+from telegram.ext import ConversationHandler
+from telegram.ext import MessageHandler
+from telegram.ext import filters
+
+
+def register(app: Application):
+    pass

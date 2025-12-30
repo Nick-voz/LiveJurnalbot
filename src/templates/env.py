adcedab1d40e55356db4a6afcb6bf4df18b18e75
Nick-voz <nik.vozisow@gmail.com>
2025-12-30 18:53:20 +0300

    Remove Jinja2 templates and replace with f-strings
    - Delete static/templates/ and src/templates/ directories
    - Remove jinja2 dependency from pyproject.toml
    - Simplify to use in-code f-strings instead of external templates

--- src/templates/env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from jinja2 import Environment
-from jinja2 import FileSystemLoader
-
-TEMPLATES_FOLDER = os.getenv("TEMPLATES_FOLDER")
-
-env = Environment(loader=FileSystemLoader(TEMPLATES_FOLDER))
